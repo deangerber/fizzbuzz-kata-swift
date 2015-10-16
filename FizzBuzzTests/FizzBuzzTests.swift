0e0c9335462df367598f872b04fdf9db92c88b23
Dean Gerber <dean.gerber@gmail.com>
2015-10-16 20:53:00 +0200

    Test target added to project.

---
+++ FizzBuzzTests/FizzBuzzTests.swift
@@ -0,0 +1,15 @@
+//
+//  FizzBuzzTests.swift
+//  FizzBuzzTests
+//
+//  Created by Dean Gerber on 2015/10/16.
+//  Copyright © 2015 deangerber. All rights reserved.
+//
+
+import XCTest
+
+class FizzBuzzTests: XCTestCase {
+  func test_framework_functions_correctly() {
+    XCTAssertTrue(true)
+  }
+}
